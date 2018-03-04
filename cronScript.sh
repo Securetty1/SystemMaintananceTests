0c05d3c006cf6d344aeece58bf510a03195ef421
Kieren Estes <36648744+Securetty1@users.noreply.github.com>
2018-03-04 13:42:05 +0100

    Update cronScript.sh

--- cronScript.sh
+++ cronScript.sh
@@ -8,6 +8,7 @@ file=~/results/02_crontabFound.txt
 echo "Enter the number of users"
 read usrNum
 
+#Creates an arrya containing all users to find crontabs for. It also 'id's them to see if the users exists - typo-proof.
 echo "Enter users separated by spaces"
 read -a usrArray
 for r in `seq 1 ${#usrArray[@]}` ; do
@@ -31,19 +32,20 @@ done
 #echo "done"
 #echo "${usrArray[@]}"
 
+date >> $file
+echo "" >> $file
 
+#Checks crontabs for each user in the 'usrArray'.
 for i in `seq 1 ${#usrArray[@]}` ; do
 	let N=i-1
 	sudo crontab -u ${usrArray[N]} -l 2>/dev/null >> /dev/null
 	if [ $? != 0 ] ; then
 		echo "No crontabs for ${usrArray[N]}"
 	else
-		date >> $file
-		echo "" >> $file
 		echo "${usrArray[N]}" >> $file
-		echo "--------------" >> $file
 		echo "Running crontabs for ${usrArray[N]}"
 	fi
 done
 
+echo "--------------" >> $file
 echo "The users who have running crontabs can be found in $file"
