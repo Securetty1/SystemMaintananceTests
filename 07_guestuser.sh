3740d20073d73e9ce4bcd568f573d7f282f5cda4
Kieren Estes <36648744+Securetty1@users.noreply.github.com>
2018-03-05 23:18:28 +0100

    Update 07_guestuser.sh

--- 07_guestuser.sh
+++ 07_guestuser.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 if [ -s 'allow-guest' /usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf ] 
-	then 
-		if [-s 'allow-guest=true' /usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf ] 
+	then
+		if [ -s 'allow-guest=true' /usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf ] 
 			then 
 				sed -i 's/allow-guest=true/allow-guest=false/' /usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf
-		else 
-			echo "Guest account has been disabled"
-		fi 
-	else 
+				echo "Guest account has been disabled"
+		fi
+	else
 		echo "allow-guest=false" >> /usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf
 fi
 
