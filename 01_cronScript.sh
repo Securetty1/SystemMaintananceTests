9f47bd83a8ec3ef646cf218da591e844460328ac
Kieren Estes <36648744+Securetty1@users.noreply.github.com>
2018-03-05 21:11:29 +0100

    Update 01_cronScript.sh

--- 01_cronScript.sh
+++ 01_cronScript.sh
@@ -4,15 +4,15 @@
 #The date and time are included in the file as well as the new scans appended (instead of overwriting the existing file)
 #in order to log activities.
 
-files=(~/results/01_crontabFound.txt)
+files=(~/results/01_crontabsFound.txt)
 
 #Creates an arrya containing all users to find crontabs for. It also 'id's them to see if the users exists - typo-proof.
 echo "Enter users separated by spaces"
 read -a usrArray
-echo ${usrArray[@]}
+#echo ${usrArray[@]}
 for r in `seq 1 ${#usrArray[@]}` ; do
 #	echo $r
-	let m=$r-1
+	let m=r-1
 #	echo $m
 #	echo ${usrArray[r]}
 #	echo ${usrArray[m]}
@@ -20,7 +20,7 @@ for r in `seq 1 ${#usrArray[@]}` ; do
 #	echo $?
 #	id ${usrArray[$r]} >> /dev/null
 #	echo $?
-	id ${usrArray[r]} 2>/dev/null >> /dev/null
+	id ${usrArray[r]} 2>/dev/null 1>/dev/null
 	if [ $? != 0 ] ; then
 		echo "No such user. Try again"
 		exit
@@ -31,20 +31,20 @@ done
 #echo "done"
 #echo "${usrArray[@]}"
 
-date >> $file
-echo "" >> $file
+date >> $files
+echo "" >> $files
 
 #Checks crontabs for each user in the 'usrArray'.
 for i in `seq 1 ${#usrArray[@]}` ; do
 	let N=i-1
-	sudo crontab -u ${usrArray[N]} -l 2>/dev/null >> /dev/null
+	sudo crontab -u ${usrArray[N]} -l 2>/dev/null 1>/dev/null
 	if [ $? != 0 ] ; then
 		echo "No crontabs for ${usrArray[N]}"
 	else
-		echo "${usrArray[N]}" >> $file
+		echo "${usrArray[N]}" >> $files
 		echo "Running crontabs for ${usrArray[N]}"
 	fi
 done
 
-echo "--------------" >> $file
-echo "The users who have running crontabs can be found in $file"
+echo "--------------" >> $files
+echo "The users who have running crontabs can be found in $files"
