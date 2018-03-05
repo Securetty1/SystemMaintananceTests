7db07f66e32f4f0a5faafe0032d135d69b256830
Kieren Estes <36648744+Securetty1@users.noreply.github.com>
2018-03-05 11:39:43 +0100

    Update 05_chmod.sh

--- 05_chmod.sh
+++ 05_chmod.sh
@@ -1,36 +1,34 @@
 #!/bin/bash
 
-echo "Identifying file permissions of critical files"
+echo "Enter password"
+sudo su -
+
+echo "Indentify/secure file permissions of critical files"
 
 chmod 640 /etc/passwd
-chown root /etc/passwd
-chgrp root /etc/passwd
+chown root:root /etc/passwd
  
-echo "Password file was and is secure"
+echo "/etc/passwd file and is secure"
 
 chmod 640 /etc/gshadow 
-chown root /etc/gshadow
-chgrp shadow /etc/gshadow
+chown root:root /etc/gshadow
 
-echo "Gshadow file was and is secure" 
+echo "/etc/gshadow file and is secure" 
 
 chmod 640 /etc/shadow 
-chown root /etc/shadow
-chgrp shadow /etc/shadow
+chown root:root /etc/shadow
  
-echo "Shadow file was and is secure"
+echo "/etc/shadow file and is secure"
 
 chmod 640 /etc/sudoers
-chown root /etc/sudoers
-chgrp root /etc/sudoers
+chown root:root /etc/sudoers
 
-echo "Sudoers file is secure"
+echo "/etc/sudoers file is secure"
 
 chmod 640 /etc/group
-chown root /etc/group
-chgrp root /etc/group 
+chown root:root /etc/group
 
-echo "Group file is secure"
+echo "/etc/group file is secure"
 
 chmod 700 /bin/ping
 chmod 700 /usr/bin/who
@@ -45,5 +43,7 @@ chmod 700 /usr/bin/gcc
 chmod 700 /usr/bin/make
 chmod 700 /usr/bin/apt-get 
 chmod 700 /usr/bin/aptitude
-
+ 
 echo "Misc functions have been locked to root ownership"
+
+#New code: search for files with dogy file permissions:
